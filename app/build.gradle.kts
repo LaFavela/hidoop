737e35b768959700338348002add517004670b03
DzulhiRaihan <dzulhiraihan@gmail.com>
2024-01-08 00:06:34 +0800

    fix issues on text input container

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,4 +74,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.compose.ui:ui-text-google-fonts:1.5.4")
+    implementation("io.coil-kt:coil-compose:2.5.0")
 }
